834349310823f66a07c030736a939d2bf8d0688f
LucasGSAntunes <190091681@aluno.unb.br>
2024-08-23 17:04:25 -0300

    refactor: Add jwtConfig module for handling JWT configuration and functionality

--- src/managers/UsuarioManager.js
+++ src/managers/UsuarioManager.js
@@ -1,5 +1,6 @@
 const { Usuario: UsuarioModel } = require("../models/Usuario");
 const bcrypt = require('bcrypt');
+const jwtConfig = require("../config/jwtConfig");
 
 const UsuarioManager = {
     createUser: async (user) => {
@@ -7,12 +8,24 @@ const UsuarioManager = {
         const hash_password = await bcrypt.hash(user.senha, salt);
         user.senha = hash_password;
         const newUser = new UsuarioModel(user);
+        
         return await newUser.save();
     },
 
-    getUserByToken: async (token) => {
-        return await UsuarioModel.findOne({ token });
+    listUsers: async () => {
+        return await UsuarioModel.find();
+    },
+
+    getUserBy: async (field, value) => {
+        return await UsuarioModel.findOne({ [`${field}`]: value });
+    },
+
+    decodeToken: (token) => {
+        token = token.split(' ')[1];
+        return jwtConfig.decodeToken(token);
     }
+
+
 };
 
 module.exports = UsuarioManager;
