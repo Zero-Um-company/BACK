7e89c5954e0a4e1c9b0c22bcefb3fafe830a57ab
LucasGSAntunes <190091681@aluno.unb.br>
2024-08-23 17:04:19 -0300

    refactor: Add listar_usuarios method to UsuarioController

--- src/controllers/UsuarioController.js
+++ src/controllers/UsuarioController.js
@@ -10,9 +10,18 @@ const UsuarioController = {
         }
     },
 
+    listar_usuarios: async (req, res) => {
+        try {
+            const usuarios = await UsuarioService.listarUsuarios();
+            res.status(200).send({ success: true, usuarios });
+        } catch (error) {
+            res.status(400).send({ success: false, message: error.message });
+        }
+    },
+
     get_me: async (req, res) => {
         try {
-            const usuario = await UsuarioService.getUserByToken(req.token);
+            const usuario = await UsuarioService.getUserByToken(req.headers.authorization);
             res.status(200).send({ success: true, usuario });
         } catch (error) {
             res.status(400).send({ success: false, message: error.message });
