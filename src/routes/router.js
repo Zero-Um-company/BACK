c197b11d73a90d51eeec27070706ae5b9cb51139
LucasGSAntunes <190091681@aluno.unb.br>
2024-08-23 10:08:04 -0300

    chore: Add login route to handle user login functionality

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,9 +1,10 @@
 const router = require('express').Router();
 
 const usuarioRouter = require('./usuarioRouter');
+const loginRouter = require('./loginRouter');
 
 router.use('/usuario', usuarioRouter);
-
+router.use('/login', loginRouter);
 
 
 module.exports = router;
