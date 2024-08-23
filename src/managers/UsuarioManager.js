7b4cf6debdb730afbfd81e634709fad331b566ff
LucasGSAntunes <190091681@aluno.unb.br>
2024-08-23 08:06:45 -0300

    Refactor package.json and UsuarioManager.js

--- src/managers/UsuarioManager.js
+++ src/managers/UsuarioManager.js
@@ -1,7 +1,11 @@
 const { Usuario: UsuarioModel } = require("../models/Usuario");
+const bcrypt = require('bcrypt');
 
 const UsuarioManager = {
     createUser: async (user) => {
+        const salt = await bcrypt.genSalt(10);
+        const hash_password = await bcrypt.hash(user.senha, salt);
+        user.senha = hash_password;
         const newUser = new UsuarioModel(user);
         return await newUser.save();
     }
