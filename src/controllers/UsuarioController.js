219ccdce094e4a7f94e09994d74fa1709e4dc4a1
LucasGSAntunes <190091681@aluno.unb.br>
2024-08-23 10:07:17 -0300

    refactor: Update UsuarioController to use UsuarioService

--- src/controllers/UsuarioController.js
+++ src/controllers/UsuarioController.js
@@ -1,16 +1,23 @@
-const UsuarioManager = require("../managers/UsuarioManager");
+const UsuarioService = require('../services/UsuarioService');
 
 const UsuarioController = {
     criar_usuario: async (req, res) => {
-        await UsuarioManager.createUser(req.body)
-        .then((usuario) => {
+        try {
+            const usuario = await UsuarioService.criarUsuario(req.body);
             res.status(201).send({ success: true, message: 'Usuário criado com sucesso', usuario });
-        })
-        .catch((error) => {
+        } catch (error) {
             res.status(400).send({ success: false, message: error.message });
-        })
+        }
+    },
 
+    get_me: async (req, res) => {
+        try {
+            const usuario = await UsuarioService.getUserByToken(req.token);
+            res.status(200).send({ success: true, usuario });
+        } catch (error) {
+            res.status(400).send({ success: false, message: error.message });
+        }
     }
-}
+};
 
-module.exports = UsuarioController
+module.exports = UsuarioController;
