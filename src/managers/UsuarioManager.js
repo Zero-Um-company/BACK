4c311c7d778faac92caf7c1e61b12f4e152279f6
LucasGSAntunes <190091681@aluno.unb.br>
2024-08-26 16:51:39 -0300

    refactor: Update UsuarioManager to use UsuarioDTO for handling user data transfer objects

--- src/managers/UsuarioManager.js
+++ src/managers/UsuarioManager.js
@@ -1,31 +1,36 @@
 const { Usuario: UsuarioModel } = require("../models/Usuario");
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
 const jwtConfig = require("../config/jwtConfig");
+const UsuarioDTO = require("../dtos/UsuarioDTO");
 
 const UsuarioManager = {
-    createUser: async (user) => {
-        const salt = await bcrypt.genSalt(10);
-        const hash_password = await bcrypt.hash(user.senha, salt);
-        user.senha = hash_password;
-        const newUser = new UsuarioModel(user);
-        
-        return await newUser.save();
-    },
+  createUser: async (userDTO) => {
+    const user = new UsuarioDTO(userDTO);
 
-    listUsers: async () => {
-        return await UsuarioModel.find();
-    },
-
-    getUserBy: async (field, value) => {
-        return await UsuarioModel.findOne({ [`${field}`]: value });
-    },
-
-    decodeToken: (token) => {
-        token = token.split(' ')[1];
-        return jwtConfig.decodeToken(token);
+    const validationErrors = UsuarioDTO.validate(user);
+    if (validationErrors.length > 0) {
+      throw new Error(`Erro de validação: ${validationErrors.join(", ")}`);
     }
 
+    const salt = await bcrypt.genSalt(10);
+    user.senha = await bcrypt.hash(user.senha, salt);
 
+    const newUser = new UsuarioModel(user);
+    return await newUser.save();
+  },
+
+  listUsers: async () => {
+    return await UsuarioModel.find();
+  },
+
+  getUserBy: async (field, value) => {
+    return await UsuarioModel.findOne({ [`${field}`]: value });
+  },
+
+  decodeToken: (token) => {
+    token = token.split(" ")[1];
+    return jwtConfig.decodeToken(token);
+  },
 };
 
 module.exports = UsuarioManager;
