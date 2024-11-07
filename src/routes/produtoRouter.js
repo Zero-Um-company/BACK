1f374b368f2393b9feb78f1305da56322fbc8b1e
artrsousa1 <artrsousa1@gmail.com>
2024-11-06 23:04:07 -0300

    feat: create new route and add jwt middlewares

--- src/routes/produtoRouter.js
+++ src/routes/produtoRouter.js
@@ -1,12 +1,14 @@
 const express = require('express');
+const jwtConfig = require('../config/JWTconfig');
 const ProdutoController = require('../controllers/produtoController');
 
 const produtoRouter = express.Router();
 
-produtoRouter.post('/criar', async (req,res) => await ProdutoController.criar_produto(req,res));
-produtoRouter.delete('/deletar/:id', async (req,res) => await ProdutoController.deletar_produto(req,res));
-produtoRouter.put('/atualizar/:id', async (req,res) => await ProdutoController.atualizar_produto(req,res));
-produtoRouter.put('/responsaveis/editar/:id', async (req,res) => await ProdutoController.editar_responsaveis(req,res));
-produtoRouter.get('/buscar', async (req,res) => await ProdutoController.buscar_produtos(req,res));
+produtoRouter.post('/criar', jwtConfig.jwtMiddleware, async (req,res) => await ProdutoController.criar_produto(req,res));
+produtoRouter.delete('/deletar/:id', jwtConfig.jwtSupervisorMiddleware, async (req,res) => await ProdutoController.deletar_produto(req,res));
+produtoRouter.put('/atualizar/:id', jwtConfig.jwtMiddleware, async (req,res) => await ProdutoController.atualizar_produto(req,res));
+produtoRouter.put('/responsaveis/editar/:id', jwtConfig.jwtSupervisorMiddleware, async (req,res) => await ProdutoController.editar_responsaveis(req,res));
+produtoRouter.get('/buscar', jwtConfig.jwtMiddleware , async (req,res) => await ProdutoController.buscar_produtos(req,res));
+produtoRouter.patch('/switch/:id', jwtConfig.jwtMiddleware, async (req,res) => await ProdutoController.altera_status(req,res));  
 
 module.exports = produtoRouter;
