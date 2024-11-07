83dd6b07d2a031174821cb65e1708d4154ff7fdb
artrsousa1 <artrsousa1@gmail.com>
2024-11-06 23:04:39 -0300

    feat: add the toggle service

--- src/services/produtoService.js
+++ src/services/produtoService.js
@@ -1,9 +1,11 @@
 const ProdutoManager = require("../managers/produtoManager");
+const mongoose = require("mongoose");
 
 const ProdutoService = {
     criarProduto: async (req) => {
         try {
-            const product = await ProdutoManager.createProduct(req.body);
+            const product = await ProdutoManager.createProduct(req.body, req.user.id);
+            
             return product;
         } catch (error) {
             throw new Error(`Erro ao criar produto: ${error.message}`);
@@ -11,18 +13,20 @@ const ProdutoService = {
     },
     editarProduto: async (req) => {
         try {
+            const user = req.user;
             const { id } = req.params;
             const { product, action } = req.body;
-            return await ProdutoManager.updateProduct(id, product);
-            // return await ProdutoManager.updateHistory(id, action);
+            await ProdutoManager.updateProduct(user, id, product);
+            return await ProdutoManager.updateHistory(id, action, req.user.id);
         } catch (error) {
             throw new Error(`Erro ao editar produto: ${error.message}.`);
         }
     },
     deletarProduto: async (req) => {
         try {
+            const user = req.user;
             const { id } = req.params;
-            return await ProdutoManager.deleteProduct(id);
+            return await ProdutoManager.deleteProduct(user, id);
         } catch (error) {
             throw new Error(`Erro ao deletar produto: ${error.message}.`);
         }
@@ -31,17 +35,28 @@ const ProdutoService = {
         try {
             const { id } = req.params;
             const { responsaveis, remove } = req.body;
-            return await ProdutoManager.updateResponsible(id,responsaveis,remove);
+            await ProdutoManager.updateResponsible(req.user, id, responsaveis, remove);
+            return await ProdutoManager.updateHistory(id, "Atualização de responsáveis.", req.user.id);
         } catch (error) {
             throw new Error(`Erro ao editar responsáveis: ${error.message}`);
         }
     },
     getProdutos: async (req) => {
-        const { userId } = req.body;
+        const user = req.user;
         try {
-            return await ProdutoManager.getAllProducts(userId);
+            return await ProdutoManager.getAllProducts(user);
         } catch (error) {
             throw new Error(`Erro ao buscar produtos: ${error.message}`);
+        }
+    },
+    alteraStatus: async (req) => {
+        try {
+            const { id } = req.params;
+            const user = req.user
+            const product = await ProdutoManager.toggleProductStatus(user, id);
+            return await ProdutoManager.updateHistory(id, "Mudança de status.", req.user.id);
+        } catch (error) {
+            throw new Error(`Erro ao mudar status: ${error.message}`);
         }
     }
 };
