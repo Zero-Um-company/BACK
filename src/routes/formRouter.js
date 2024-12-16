2e209baa0211f8aee6c7a9e9fbf2c2c9a4a7fbe5
Arthur Ribeiro <artrsousa1@gmail.com>
2024-12-16 15:29:44 -0300

    feat(route): add middleware to all routes

--- src/routes/formRouter.js
+++ src/routes/formRouter.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const formController = require('../controllers/formController');
+const jwtConfig = require('../config/JWTconfig');
 
 const formRouter = express.Router();
 
-formRouter.post('/criar', async (req, res) =>  await formController.criarForm(req, res));
-formRouter.get('/:id', async (req, res) => await formController.getFormById(req, res));
-formRouter.get('/', async (req, res) => await formController.listAllForms(req,res));
-formRouter.put('/editar/:id', async (req, res) => await formController.editarForm(req, res));
-formRouter.delete('/deletar/:id', async (req, res) => await formController.deletarForm(req, res));
+formRouter.post('/criar', jwtConfig.jwtMiddleware,  async (req, res) =>  await formController.criarForm(req, res));
+formRouter.get('/:id', jwtConfig.jwtMiddleware, async (req, res) => await formController.getFormById(req, res));
+formRouter.get('/', jwtConfig.jwtMiddleware, async (req, res) => await formController.listAllForms(req,res));
+formRouter.put('/editar/:id', jwtConfig.jwtMiddleware, async (req, res) => await formController.editarForm(req, res));
+formRouter.delete('/deletar/:id', jwtConfig.jwtMiddleware, async (req, res) => await formController.deletarForm(req, res));
 
 module.exports = formRouter;
